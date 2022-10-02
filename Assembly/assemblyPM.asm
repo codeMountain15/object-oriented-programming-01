ca24f7d7ebda8a5d6bba6f82e6e84cff92f82a17
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-02 15:51:45 +0300

    Update assemblyPM.asm

--- Assembly/assemblyPM.asm
+++ Assembly/assemblyPM.asm
@@ -7,9 +7,9 @@ INCLUDE Irvine32.inc
 .code
 
 main PROC
-  mov eax,30000h ; EAX = 30000h
-  add eax,10000h ; EAX = 40000h
-  sub eax,20000h ; EAX = 20000h
+  mov eax,30000h      ; EAX == 30000h
+  add eax,10000h      ; EAX == 40000h
+  sub eax,20000h      ; EAX == 20000h
   call DumpRegs
   exit
 
