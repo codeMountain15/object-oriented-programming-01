192c1b4d02e5ac3824653dcdc1d58b5a3fb2de84
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-02 13:39:34 +0300

    Create example01.lisp

---
+++ Lisp/example01.lisp
@@ -0,0 +1,12 @@
+;check number if it is odd or not
+(write (evenp 45))
+(terpri)
+
+
+;check number if it is odd or not
+(write (evenp 60))
+(terpri)
+
+
+;It will return **T** if the number is even otherwise **NIL**
+;terpri = TERminate PRInt line
