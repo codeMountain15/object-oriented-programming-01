0aa46fdc6a3088e2039f9221ef31692ce1bfb2a2
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-04 21:51:49 +0300

    Add files via upload

---
+++ C/add01.c
@@ -0,0 +1,16 @@
+#include<stdio.h>    
+
+int main(){    
+int x=0,y=0,result=0;  
+  
+printf("enter first number: ");  
+scanf("%d",&x);  
+
+printf("enter second number: ");  
+scanf("%d",&y);  
+  
+result=x+y;  
+printf("sum of 2 numbers:%d ",result);  
+  
+return 0;  
+}    
