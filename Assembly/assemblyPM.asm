7170efddc278e86b511c03acaac56b6c13aeb99c
Diogenis Alexandrakis <diogenis_Al@yahoo.com>
2022-10-13 23:05:53 +0300

    Update assemblyPM.asm

--- Assembly/assemblyPM.asm
+++ Assembly/assemblyPM.asm
@@ -10,8 +10,10 @@ main PROC             ; starts the procedure
   mov eax,30000h      ; EAX == 30000h
   add eax,10000h      ; EAX == 40000h
   sub eax,20000h      ; EAX == 20000h
-  call DumpRegs
-  exit
+  
+  call DumpRegs       ; it displays the registers - sort of output
+  
+  exit                ; halts the program
 
 main ENDP       ;end of the procedure	
 END main        ;end of the program
